ddf6422cae6b1b1d414978a7fb31eb0ca4eed31d
Tetsuya Minase <tetsuya.minasen@gmail.com>
2020-08-07 01:06:52 +0900

    型定義追加、全体リファクタ (#2)
    * feat: add styled-components.📚
    
    * feat: add styled-components type definition.📚
    
    * refactor: use styled-components in HeaderComponent.🐥
    
    * refactor: to ImageComponent.🐥
    
    * feat: add types and refactoring.🐔

--- src/templates/PageTemplate.tsx
+++ src/templates/PageTemplate.tsx
@@ -1,18 +1,14 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
-import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
-import "./layout.css"
+import { HeaderComponent } from '../components/HeaderComponent'
+import "./pageTemplate.css"
 
-const Layout: React.FC<any> = ({ children }) => {
+type Props = {
+  children: JSX.Element;
+}
+
+export const PageTemplate: React.FC<Props> = ({ children }) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -25,7 +21,7 @@ const Layout: React.FC<any> = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
+      <HeaderComponent siteTitle={data.site.siteMetadata.title} />
       <div
         style={{
           margin: `0 auto`,
@@ -43,9 +39,3 @@ const Layout: React.FC<any> = ({ children }) => {
     </>
   )
 }
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
-
-export default Layout
