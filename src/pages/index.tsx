a4bed46c9628da0cd7515faeffcef432e5deacc9
Tetsuya Nishi <tetsuya.minasen@gmail.com>
2020-08-08 01:29:27 +0900

    refactor: move reset-css.🚚

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,21 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react';
+import { Link } from 'gatsby';
 
-import { PageTemplate } from "../templates/PageTemplate"
-import { Image } from "../components/ImageComponent"
-import SEO from "../components/seo"
+import { PageTemplate } from '../templates/PageTemplate';
+import { Image } from '../components/ImageComponent';
+import SEO from '../components/seo';
 
 const IndexPage = () => (
   <PageTemplate>
     <React.Fragment>
-      <SEO title="Home"/>
+      <SEO title="Home" />
       <h1>Hi people</h1>
       <p>Welcome to your new Gatsby site.</p>
       <p>Now go build something great.</p>
       <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-        <Image/>
+        <Image />
       </div>
-      <Link to="/page-2/">Go to page 2</Link> <br/>
+      <Link to="/page-2/">Go to page 2</Link> <br />
       <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
     </React.Fragment>
   </PageTemplate>
