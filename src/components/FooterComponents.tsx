d7788c56a0e5968ea1361077c5f60ab5dda39bb6
Tetsuya Nishi <tetsuya.minasen@gmail.com>
2020-08-07 23:18:50 +0900

    feat: add FooterComponent.🥚

---
+++ src/components/FooterComponents.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Footer = styled.footer`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  background-color: #e9ecef;
+`;
+const Small = styled.small`
+  font-size: 1.2rem;
+  margin-bottom: 0.5rem;
+`;
+
+export const FooterComponent: React.FC = () => (
+  <Footer>
+    <Small>&copy; Tetsuya Minase</Small>
+    <Small>
+      &copy;{new Date().getFullYear()}, Built with
+      <a href="https://www.gatsbyjs.org">Gatsby</a>
+    </Small>
+  </Footer>
+);
