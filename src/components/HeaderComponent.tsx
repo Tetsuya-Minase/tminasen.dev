5e1c62eed3fe7f652028f9fbd9a878dd2b07efc3
Tetsuya Minase <tetsuya.minasen@gmail.com>
2020-08-08 01:44:35 +0900

    Header/Footer修正＋SEO微修正 (#5)
    * feat: add reset-css.🧹
    
    * feat: add header base style.🎨
    
    * feat: update header title font-size.🐔
    
    * style: update prettierrc.🐔
    
    * feat: add FooterComponent.🥚
    
    * refactor: css properties.🐥
    
    * refactor: adjust pagetemplate.🐔
    
    * refactor: move reset-css.🚚
    
    * feat: update seo.tsx.🐔

--- src/components/HeaderComponent.tsx
+++ src/components/HeaderComponent.tsx
@@ -1,35 +1,34 @@
-import { Link } from "gatsby"
-import React from "react"
+import { Link } from 'gatsby';
+import React from 'react';
 import styled from 'styled-components';
 
 type Props = {
   siteTitle: string;
-}
+};
 
 const Header = styled.header`
-  background: rebeccapurple;
+  background: #448aff;
   margin-bottom: 1.45rem;
 `;
 const Heading = styled.h1`
   margin: 0;
+  font-size: 3rem;
 `;
 const Wrapper = styled.div`
   margin: 0 auto;
-  max-width: 960px;
-  padding: 1.45rem 1.0875rem;
+  max-width: 96rem;
+  padding: 1.5rem 1.1rem;
 `;
+const linkStyle: React.CSSProperties = {
+  color: '#ffffff',
+  textDecoration: 'none',
+};
 
 export const HeaderComponent: React.FC<Props> = ({ siteTitle }) => (
   <Header>
     <Wrapper>
       <Heading>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
+        <Link to="/" style={linkStyle}>
           {siteTitle}
         </Link>
       </Heading>
--- src/templates/PageTemplate.tsx
+++ src/templates/PageTemplate.tsx
@@ -1,12 +1,25 @@
-import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
+import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
 
-import { HeaderComponent } from '../components/HeaderComponent'
-import "./pageTemplate.css"
+import { HeaderComponent } from '../components/HeaderComponent';
+import { FooterComponent } from '../components/FooterComponents';
+import styled from 'styled-components';
 
 type Props = {
   children: JSX.Element;
-}
+};
+
+const BodyWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`;
+const MainWrapper = styled.div`
+  flex: 1 0 auto;
+  margin: 0 auto;
+  max-width: 9.6rem;
+  padding: 0 1.1rem 1.5rem;
+`;
 
 export const PageTemplate: React.FC<Props> = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -17,12 +30,12 @@ export const PageTemplate: React.FC<Props> = ({ children }) => {
         }
       }
     }
-  `)
+  `);
 
   return (
-    <>
+    <BodyWrapper>
       <HeaderComponent siteTitle={data.site.siteMetadata.title} />
-      <div
+      <MainWrapper
         style={{
           margin: `0 auto`,
           maxWidth: 960,
@@ -30,12 +43,8 @@ export const PageTemplate: React.FC<Props> = ({ children }) => {
         }}
       >
         <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    </>
-  )
-}
+      </MainWrapper>
+      <FooterComponent />
+    </BodyWrapper>
+  );
+};
