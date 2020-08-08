21a489b531a2aa90f5954eb019125e3b7f8006c2
Tetsuya Nishi <tetsuya.minasen@gmail.com>
2020-08-08 21:46:41 +0900

    feat: add article lists.🐥

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,70 @@
-import React from 'react';
-import { Link } from 'gatsby';
+import React, { useMemo } from 'react';
+import { graphql, Link } from 'gatsby';
 
 import { PageTemplate } from '../templates/PageTemplate';
 import { Image } from '../components/ImageComponent';
 import SEO from '../components/seo';
+import { IndexPageQuery } from '../../types/graphql-types';
 
-const IndexPage = () => (
-  <PageTemplate>
-    <React.Fragment>
-      <SEO title="Home" />
-      <h1>Hi people</h1>
-      <p>Welcome to your new Gatsby site.</p>
-      <p>Now go build something great.</p>
-      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-        <Image />
-      </div>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </React.Fragment>
-  </PageTemplate>
-);
+type Props = {
+  data: IndexPageQuery;
+};
+
+const useCreateArticle = (
+  articleData: IndexPageQuery['allMarkdownRemark']['edges'],
+) => {
+  return useMemo(() => {
+    const linkItems = articleData
+      .map(item => {
+        const frontmatter = item.node.frontmatter;
+        if (frontmatter?.path == undefined || frontmatter?.title == undefined) {
+          return undefined;
+        }
+        return (
+          <li key={`${frontmatter.title}:${frontmatter.path}`}>
+            <Link to={frontmatter.path}>{frontmatter.title}</Link>
+          </li>
+        );
+      })
+      .filter((item): item is JSX.Element => item !== undefined);
+    if (linkItems.length === 0) {
+      return null;
+    }
+    return <ul>{linkItems}</ul>;
+  }, [articleData.length]);
+};
+
+const IndexPage: React.FC<Props> = ({ data }) => {
+  const articles = useCreateArticle(data.allMarkdownRemark.edges);
+  return (
+    <PageTemplate>
+      <React.Fragment>
+        <SEO title="Home" />
+        <h1>Hi people</h1>
+        <p>Welcome to your new Gatsby site.</p>
+        <p>Now go build something great.</p>
+        <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+          <Image />
+        </div>
+        {articles}
+      </React.Fragment>
+    </PageTemplate>
+  );
+};
 
 export default IndexPage;
+
+export const pageQuery = graphql`
+  query IndexPage {
+    allMarkdownRemark {
+      edges {
+        node {
+          frontmatter {
+            path
+            title
+          }
+        }
+      }
+    }
+  }
+`;
