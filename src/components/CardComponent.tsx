162e7905db7bee009b0f26fe0651f846f2a008ab
Tetsuya Nishi <tetsuya.minasen@gmail.com>
2020-10-02 22:55:43 +0900

    feat: add card component.🐥

---
+++ src/components/CardComponent.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import styled from 'styled-components';
+import { DeepReadonly } from '../../types/utility';
+import { bodyFontSize, fontColor, headerFontSize } from '../styles/variable';
+
+const Article = styled.article`
+  display: flex;
+  flex-direction: column;
+  width: 35rem;
+`;
+const Title = styled.div`
+  font-size: ${headerFontSize.h1};
+`;
+const Image = styled.img``;
+const Description = styled.div`
+  font-size: ${bodyFontSize.medium};
+`;
+const Link = styled.a`
+  color: ${fontColor.black};
+  text-decoration: none;
+`;
+
+type Props = {
+  title: string;
+  path: string;
+  excerpt: string;
+};
+
+export const CardComponent: React.FC<DeepReadonly<Props>> = ({
+  title,
+  path,
+  excerpt,
+}) => {
+  return (
+    <Article>
+      <Link href={path}>
+        <Image width="350px" height="200px" />
+        <Title>{title}</Title>
+        <Description>{excerpt}</Description>
+      </Link>
+    </Article>
+  );
+};
