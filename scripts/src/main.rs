23624e7f26da6fe3817347cd01869ad5ba6d4914
Tetsuya Nishi <tetsuya.minasen@gmail.com>
2021-02-16 22:53:52 +0900

    feat: use hexadecimal for article paths.

--- scripts/src/main.rs
+++ scripts/src/main.rs
@@ -1,17 +1,17 @@
-use std::iter;
+use chrono::Local;
+use clap::{App, Arg, ArgMatches};
+use rand::{thread_rng, Rng};
 use std::fs;
 use std::fs::File;
 use std::io;
 use std::io::Write;
-use rand::{thread_rng, Rng};
-use rand::distributions::Alphanumeric;
-use chrono::Local;
-use clap::{App, Arg, ArgMatches};
 
-/// 
+const BASE_STRING: &[u8] = b"1234567890abcdef";
+
+///
 /// file output
-/// 
-/// * `s` - text 
+///
+/// * `s` - text
 /// * `path` - output path
 ///
 fn write(s: &str, path: &str) -> io::Result<()> {
@@ -20,17 +20,21 @@ fn write(s: &str, path: &str) -> io::Result<()> {
     f.write_all(s.as_bytes())
 }
 
-
 ///
 /// create template files.
 /// * `matches` - information about arguments.
-/// 
+///
 fn create_file(matches: ArgMatches) {
     let file_name: String = match matches.value_of("file-name") {
         Some(name) => name.to_string(),
         None => {
             let mut rng = thread_rng();
-            let rand_string: String = iter::repeat(()).map(|()| rng.sample(Alphanumeric)).take(20).collect();
+            let rand_string: String = (0..30)
+                .map(|_| {
+                    let index = rng.gen_range(0..BASE_STRING.len());
+                    BASE_STRING[index] as char
+                })
+                .collect();
             rand_string.to_lowercase()
         }
     };
@@ -44,13 +48,17 @@ fn create_file(matches: ArgMatches) {
         println!("! {:?}", why.kind());
     });
 
-    let template_data = format!("---\n\
+    let template_data = format!(
+        "---\n\
     path: \"/blog/{}\"\n\
     date: \"{}\"\n\
     title: \"\"\n\
     tag: [\"\"]\n\
     thumbnailImage: \"/images/\"\n\
-    ---", file_name, Local::now().format("%Y/%m/%d").to_string());
+    ---",
+        file_name,
+        Local::now().format("%Y/%m/%d").to_string()
+    );
 
     let markdown_path: String = format!("src/md-pages/{}/article{}.md", file_name, file_name);
     write(&template_data, &markdown_path).unwrap_or_else(|why| {
@@ -68,7 +76,7 @@ fn create_file(matches: ArgMatches) {
 fn rename_images(matches: ArgMatches) -> io::Result<()> {
     let dir_name: String = match matches.value_of("rename") {
         Some(name) => name.to_string(),
-        _ => "".to_string()
+        _ => "".to_string(),
     };
 
     let mut index = 1;
@@ -83,7 +91,10 @@ fn rename_images(matches: ArgMatches) -> io::Result<()> {
         })
         .for_each(move |file| {
             let before_filename = format!("public/images/article/{}/{}", dir_name, file);
-            let after_filename = format!("public/images/article/{}/ss{}-{}.png", dir_name, dir_name, index);
+            let after_filename = format!(
+                "public/images/article/{}/ss{}-{}.png",
+                dir_name, dir_name, index
+            );
             fs::rename(before_filename, after_filename);
             index += 1;
         });
@@ -95,21 +106,23 @@ fn main() {
         .version("0.0.1")
         .author("tminasen")
         .about("article utility tools")
-        .arg(Arg::with_name("file-name")
-            .short("n")
-            .long("name")
-            .value_name("FILE_NAME")
-            .help("specify file name")
-            .takes_value(true)
-            .required(false)
+        .arg(
+            Arg::with_name("file-name")
+                .short("n")
+                .long("name")
+                .value_name("FILE_NAME")
+                .help("specify file name")
+                .takes_value(true)
+                .required(false),
         )
-        .arg(Arg::with_name("rename")
-            .long("rename")
-            .value_name("TARGET_DIR_NAME")
-            .help("rename image files")
-            .takes_value(true)
-            .multiple(true)
-            .required(false)
+        .arg(
+            Arg::with_name("rename")
+                .long("rename")
+                .value_name("TARGET_DIR_NAME")
+                .help("rename image files")
+                .takes_value(true)
+                .multiple(true)
+                .required(false),
         )
         .get_matches();
 
