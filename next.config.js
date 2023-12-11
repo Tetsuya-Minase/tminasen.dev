76b85bf25db83f99ba2aa639df7634811d34537f
Tetsuya Minase <tetsuya.minasen@gmail.com>
2023-12-11 15:40:26 +0900

    update next.js (#68)
    * feat: update nextjs
    
    * feat: remove unused keys.
    
    * build: update build script.

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   swcMinify: true,
-  experimental: {
-    appDir: true,
-  },
   compiler: {
     styledComponents: true,
   },
