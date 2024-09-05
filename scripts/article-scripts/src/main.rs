927c2de78b49d8a8a9223932f83abae893625e65
Tetsuya Minase <tetsuya.minasen@gmail.com>
2024-09-05 23:02:03 +0900

    refactor: article scripts (#74)

--- scripts/article-scripts/src/main.rs
+++ scripts/article-scripts/src/main.rs
@@ -1,20 +1,8 @@
-mod features;
-
 use anyhow::Result;
-use features::cli_config::get_matches;
-use features::file_operations::create_file;
-use features::file_operations::rename_images;
+use article_scripts::{ArticleCreator, Command};
 
 fn main() -> Result<()> {
-    let matches = get_matches();
-
-    // フォーマット変更
-    if matches.contains_id("rename") {
-        rename_images(&matches)?;
-        return Ok(());
-    }
-    // それ以外はファイル作成
-    create_file(&matches)?;
-
-    Ok(())
+    let matches = ArticleCreator::cli().get_matches();
+    let creator = ArticleCreator::new();
+    creator.execute(&matches)
 }
