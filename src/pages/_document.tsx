b216bc0b6c6920f3c2df0e4540030b745bea5951
Tetsuya-Minase <tetsuya.minasen@gmail.com>
2025-04-14 01:02:36 +0900

    refactor: remove styled-components integration from custom Document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,39 +1,12 @@
 import React from 'react';
 import Document, {
-  DocumentContext,
   Html,
   Main,
   Head,
   NextScript,
 } from 'next/document';
-import { ServerStyleSheet } from 'styled-components';
-import { DocumentInitialProps } from 'next/dist/shared/lib/utils';
 
 export default class MyDocument extends Document {
-  static async getInitialProps(
-    ctx: DocumentContext,
-  ): Promise<DocumentInitialProps> {
-    const sheet = new ServerStyleSheet();
-    const originalRenderPage = ctx.renderPage;
-
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
-        });
-
-      const initialProps = await Document.getInitialProps(ctx);
-      return {
-        ...initialProps,
-        styles: [
-          ...React.Children.toArray(initialProps.styles),
-          ...sheet.getStyleElement(),
-        ],
-      };
-    } finally {
-      sheet.seal();
-    }
-  }
 
   render() {
     return (
