5739045abd992c49ed50c8b205be3ac1990897fb
Tetsuya-Minase <tetsuya.minasen@gmail.com>
2025-04-13 20:36:43 +0900

    refactor: remove styled-components from HeaderComponent and use Tailwind CSS for styling

--- src/components/HeaderComponent.tsx
+++ src/components/HeaderComponent.tsx
@@ -1,8 +1,5 @@
-import React from 'react';
-import styled from 'styled-components';
-import media from 'styled-media-query';
+import { FC } from 'react';
 import { LinkComponent } from './atoms/LinkComponent';
-import { color, fontSize, layer } from '../styles/variable';
 
 type Props = {
   siteTitle: string;
@@ -11,46 +8,46 @@ type Props = {
   closeModal: () => void;
 };
 
-const MenuDialog = styled.div<{ showModal: boolean }>`
-  display: ${({ showModal }) => (showModal ? 'block' : 'none')};
-  position: absolute;
-  left: 28px;
-  top: 8px;
-  z-index: ${layer.overlay};
-  background-color: ${color.bgWhite};
-  min-width: 500px;
-  min-height: 80vh;
-  border-radius: 4px;
-  ${media.lessThan('small')`
-    min-width: 360px;
-    left: 4px;
-  `}
-`;
-
-export const HeaderComponent: React.FC<Props> = ({
+export const HeaderComponent: FC<Props> = ({
   siteTitle,
   showModal,
   openModal,
   closeModal,
 }) => {
   return (
-    <header className='flex relative bg-(--header-bg-blue) max-h-[64px] justify-center items-center'>
-      <button className='menu-button absolute w-7 sm:w-12 left-2 sm:left-7 no-underline bg-transparent' onClick={openModal}>メニュー</button>
-      <h1 className='text-2xl sm:text-4xl font-bold leading-[1.5]'>
+    <header className="flex relative bg-(--header-bg-blue) max-h-[64px] justify-center items-center">
+      <button
+        className="menu-button absolute w-7 sm:w-12 left-2 sm:left-7 no-underline bg-transparent"
+        onClick={openModal}
+      >
+        メニュー
+      </button>
+      <h1 className="text-2xl sm:text-4xl font-bold leading-[1.5]">
         <LinkComponent url="/" color="white">
           {siteTitle}
         </LinkComponent>
       </h1>
-      <MenuDialog showModal={showModal}>
-        <button className='close-button relative border-none bg-transparent w-10 h-10' onClick={closeModal}>閉じる</button>
+      <div
+        className={`${
+          showModal ? 'block' : 'hidden'
+        } absolute left-1 sm:left-7 top-2 z-10 bg-neutral-100 min-w-[360px] sm:min-w-[500px] min-h-[75vh] rounded-sm`}
+      >
+        <button
+          className="close-button relative border-none bg-transparent w-10 h-10"
+          onClick={closeModal}
+        >
+          閉じる
+        </button>
         <ul>
-          <li className='flex justify-center'>
+          <li className="flex justify-center">
             <LinkComponent url="/tags" color="black">
-              <span className='text-2xl sm:text-4xl font-bold underline'>タグ一覧</span>
+              <span className="text-2xl sm:text-4xl font-bold underline">
+                タグ一覧
+              </span>
             </LinkComponent>
           </li>
         </ul>
-      </MenuDialog>
+      </div>
     </header>
   );
 };
