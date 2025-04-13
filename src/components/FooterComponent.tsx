228b68f3f6cce381525aacf4af5a1f33e3a03771
Tetsuya-Minase <tetsuya.minasen@gmail.com>
2025-04-13 20:51:46 +0900

    refactor: remove styled-components from FooterComponent and use Tailwind CSS for styling

--- src/components/FooterComponent.tsx
+++ src/components/FooterComponent.tsx
@@ -14,8 +14,8 @@ const Small = styled.small`
 `;
 
 export const FooterComponent: React.FC = () => (
-  <Footer>
-    <Small>&copy;{new Date().getFullYear()} tminasen</Small>
-    <Small>This site uses Google Analytics.</Small>
-  </Footer>
+  <footer className='flex items-center flex-col bg-slate-300 mt-3'>
+    <small className='text-xs mb-2'>&copy;{new Date().getFullYear()} tminasen</small>
+    <small className='text-xs mb-2'>This site uses Google Analytics.</small>
+  </footer>
 );
