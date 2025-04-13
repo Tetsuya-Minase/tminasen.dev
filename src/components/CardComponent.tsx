da5a5f03320b3f415bf4b9fd549c1e42eea588df
Tetsuya-Minase <tetsuya.minasen@gmail.com>
2025-04-13 14:32:52 +0900

    refactor: remove styled-components from CardComponent and use Tailwind CSS for styling

--- src/components/CardComponent.tsx
+++ src/components/CardComponent.tsx
@@ -1,33 +1,7 @@
 import React from 'react';
-import styled from 'styled-components';
 import { DeepReadonly } from '../../types/utility';
-import { color, fontSize, size } from '../styles/variable';
 import { Image } from './atoms/ImageComponent';
 import { ThumbnailImage } from '../../types/article';
-
-const Article = styled.article`
-  display: flex;
-  flex-direction: column;
-  width: ${size.cardImageWidth};
-  max-height: ${size.cardMaxHeight};
-  border-radius: 10px;
-  background-color: ${color.bgWhite};
-  box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3);
-`;
-const Title = styled.h1`
-  font-size: ${fontSize.h1};
-  text-align: center;
-  margin-top: 8px;
-`;
-const Description = styled.p`
-  font-size: ${fontSize.px16};
-  margin-top: 8px;
-  padding: 0 8px;
-`;
-const Link = styled.a`
-  color: ${color.textBlack};
-  text-decoration: none;
-`;
 
 type Props = {
   title: string;
