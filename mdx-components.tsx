552420b6a186fb9de7d9059f2f1bb0447a05f825
Tetsuya-Minase <tetsuya.minasen@gmail.com>
2026-04-12 09:50:30 +0900

    feat: 記事内画像のクリックで拡大表示するLightbox機能を追加

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,7 +1,9 @@
 import type { MDXComponents } from 'mdx/types';
+import { Lightbox } from './src/components/atoms/Lightbox';
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
+    img: (props) => <Lightbox {...props} />,
     a: ({ href, children, ...props }) => {
       const isExternal = href?.startsWith('http');
       return (
